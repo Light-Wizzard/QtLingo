9c6057bbfe4c24154b0eda11cdf2cc0936e364d0
Light-Wizzard <thelightwizzard@gmail.com>
2021-08-11 19:26:13 -0700

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -24,11 +24,16 @@ if [[ "$APPVEYOR_BUILD_WORKER_IMAGE" = "Ubuntu" ]]; then
     if [[ "$MY_UPGRADE_OS" = "true" ]]; then sudo apt-get upgrade -qqy; fi
     # Developer tools and libsqlite3
     sudo apt install -qqy build-essential cmake software-properties-common libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev;
-    sudo apt install -qqy libffi-dev mesa-common-dev libglu1-mesa-dev freeglut3-dev libegl1-Mesa-dev libmesa-dev;
+    sudo apt install -qqy libffi-dev mesa-common-dev libglu1-mesa-dev freeglut3-dev libegl1-Mesa-dev libmesa-dev pkg-config;
     sudo apt install -qqy libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev flex bison checkinstall libsqlite3-dev;
     sudo apt install -qqy libreadline-dev libgl-dev libpulse-mainloop-glib0 libgles2-mesa-dev libglfw3-dev libgl1-mesa-dev;
     sudo apt install -qqy libfreetype6-dev libfontconfig1-dev libdbus-1-dev libxi-dev xnviewmp libgstreamer-plugins-base1.0-0;
     sudo apt install -qqy libgstreamer0.10-0 libgstreamer-plugins-base0.10-0 libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev;
+    sudo apt install -qqy yasm git gfortran libjpeg8-dev libpng-dev libjasper1 libtiff-dev libavcodec-dev libavformat-dev;
+    sudo apt install -qqy libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libatlas-base-dev libfaac-dev libmp3lame-dev;
+    sudo apt install -qqy libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev libavresample-dev;
+    sudo apt install -qqy x264 v4l-utils libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev;
+    sudo apt install -qqy libhdf5-dev doxygen;
     # Python
     if [[ "$MY_PYTHON_REQUIRED" = "true" ]]; then sudo apt-get install -qqy python3.9-dev python3-venv; fi
     # Required by LinuxDeploy
